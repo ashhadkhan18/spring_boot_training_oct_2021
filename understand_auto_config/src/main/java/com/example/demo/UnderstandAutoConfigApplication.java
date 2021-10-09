7a4aeb752bec797a9a5a53ccad616f626b01fffd
srivatsan <vatsan_k@hotmail.com>
2021-10-09 14:46:15 +0530

    Updated App Config Class

--- understand_auto_config/src/main/java/com/example/demo/UnderstandAutoConfigApplication.java
+++ understand_auto_config/src/main/java/com/example/demo/UnderstandAutoConfigApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 
@@ -12,7 +16,15 @@ import com.example.demo.util.CustomerCreationCondtion;
 public class UnderstandAutoConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UnderstandAutoConfigApplication.class, args);
+		
+	Banner.Mode mode = Banner.Mode.OFF;
+	ConfigurableApplicationContext	ctx=new SpringApplicationBuilder(UnderstandAutoConfigApplication.class)
+			         .bannerMode(mode).build().run(args);
+	
+	  System.out.println(ctx.getBean(Customer.class));
+	  
+	  ctx.close();
+			
 	}
 
 	@Bean
@@ -25,11 +37,21 @@ public class UnderstandAutoConfigApplication {
 	}
 	
 	@Bean
+	@ConditionalOnProperty(name = "spring.application.name", havingValue = "auto")
 	public Customer suresh() {
 		
 		return new Customer(102, "Suresh", "sur@abc.com");
 		
 		
 	}
+	
+	@Bean
+	public Customer shyam() {
+		
+		return new Customer(103, "Shyam", "shy@abc.com");
+		
+		
+	}
+	
 	
 }
