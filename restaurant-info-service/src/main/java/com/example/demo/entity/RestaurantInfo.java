3cce8d76906d7359cf88fdaa7392f3b72f7e6b4c
srivatsan <vatsan_k@hotmail.com>
2021-10-10 10:10:46 +0530

    Created Entity Classes

--- restaurant-info-service/src/main/java/com/example/demo/entity/RestaurantInfo.java
+++ restaurant-info-service/src/main/java/com/example/demo/entity/RestaurantInfo.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
@@ -15,16 +17,25 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Component
+
 @Entity
 @Table(name = "restaurant_info_oct2021")
 public class RestaurantInfo {
 
+	    @Id
+	    @Column(name = "id")
 	    private int id;
+	    @Column(name = "name")
 	    private String name;
+	    @Column(name = "servicearea")
 	    private String serviceArea;
+	    @Column(name = "emailaddress")
 	    private String emailAddress;
+	    @Column(name = "menutype")
 	    private String menuType;
+	    @Column(name = "openinghours")
 	    private String openingHours;
 	    
 }
