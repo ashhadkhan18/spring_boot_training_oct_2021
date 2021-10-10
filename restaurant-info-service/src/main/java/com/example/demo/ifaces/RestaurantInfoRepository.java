82d8fd78a81ebacabb2335d713e53b76000678d4
srivatsan <vatsan_k@hotmail.com>
2021-10-10 12:51:45 +0530

    Query DSL Exercises Added

--- restaurant-info-service/src/main/java/com/example/demo/ifaces/RestaurantInfoRepository.java
+++ restaurant-info-service/src/main/java/com/example/demo/ifaces/RestaurantInfoRepository.java
@@ -1,12 +1,33 @@
 package com.example.demo.ifaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.RestaurantInfo;
+import java.util.*;
 
+import javax.transaction.Transactional;
 @Repository
 
 public interface RestaurantInfoRepository extends JpaRepository<RestaurantInfo, Integer> {
 
+	// Query DSL
+	
+	List<RestaurantInfo> findByServiceArea(String area);
+	
+	// Query DSL with native query
+	@Query(nativeQuery = true,value = "select * from restaurant_info_oct2021 where menutype=:type")
+	List<RestaurantInfo> findByMealType(@Param("type") String type);
+	
+	// Query DSL with JPQL
+	
+	@Query(value = "update RestaurantInfo set openinghours=:revisedTime")
+	@Modifying
+	@Transactional
+	int updateTiming(@Param("revisedTime") String revisedTime);
+	
+	
 }
