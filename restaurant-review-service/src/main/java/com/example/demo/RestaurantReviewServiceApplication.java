9cbf5de841efef0ef007dd8a6d08b0bdf8780792
srivatsan <vatsan_k@hotmail.com>
2021-10-10 17:00:54 +0530

    Hateos Rest API Completed

--- restaurant-review-service/src/main/java/com/example/demo/RestaurantReviewServiceApplication.java
+++ restaurant-review-service/src/main/java/com/example/demo/RestaurantReviewServiceApplication.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.ifaces.RestaurantInfoRepository;
+import com.example.demo.model.RestaurantInfo;
+import com.example.demo.model.Review;
 
 @SpringBootApplication
 public class RestaurantReviewServiceApplication {
@@ -10,4 +19,34 @@ public class RestaurantReviewServiceApplication {
 		SpringApplication.run(RestaurantReviewServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			RestaurantInfoRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+		
+				Review review1 = new Review(101, "shyam", "food", "excellent", 4.5);
+				Review review2 = new Review(102, "Shiv", "parking", "needs improvement", 2.5);
+				
+				Set<Review> reviews = new HashSet<>();
+				
+				  reviews.add(review1);
+				  reviews.add(review2);
+				  
+				  
+				  RestaurantInfo sangeetha = 
+						  new RestaurantInfo(899, "sanggetha", "chennai" , "sang@abc.com", "veg","600to88" ,reviews);
+				  
+				  
+				  repo.save(sangeetha);
+				  
+				
+			}
+		};
+	}
 }
