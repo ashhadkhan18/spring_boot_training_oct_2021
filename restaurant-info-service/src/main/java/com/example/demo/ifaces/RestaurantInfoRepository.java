f8bad1765c75bf8c7c8f55ab43bab70c3392f99a
srivatsan <vatsan_k@hotmail.com>
2021-10-10 10:20:34 +0530

    Update

--- restaurant-info-service/src/main/java/com/example/demo/ifaces/RestaurantInfoRepository.java
+++ restaurant-info-service/src/main/java/com/example/demo/ifaces/RestaurantInfoRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.ifaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RestaurantInfoRepository extends JpaRepository<T, ID> {
+import com.example.demo.entity.RestaurantInfo;
+
+@Repository
+public interface RestaurantInfoRepository extends JpaRepository<RestaurantInfo, Integer> {
 
 }
--- restaurant-info-service/src/main/java/com/example/demo/services/RestaurantInfoService.java
+++ restaurant-info-service/src/main/java/com/example/demo/services/RestaurantInfoService.java
@@ -1,5 +1,33 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+import com.example.demo.entity.RestaurantInfo;
+import com.example.demo.ifaces.RestaurantInfoRepository;
+
+@Service
 public class RestaurantInfoService {
 
+	private RestaurantInfoRepository repo;
+
+	@Autowired
+	public void setRepo(RestaurantInfoRepository repo) {
+		this.repo = repo;
+	}
+	
+	
+	public List<RestaurantInfo> findAll(){
+		
+		return this.repo.findAll();
+	}
+	
+	public RestaurantInfo add(RestaurantInfo entity) {
+		
+		return this.repo.save(entity);
+	}
+	
+	
+	
 }
