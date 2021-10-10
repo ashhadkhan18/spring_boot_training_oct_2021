d711e01dbc2890923ed3c1f0ac447a103c75c6bc
srivatsan <vatsan_k@hotmail.com>
2021-10-10 11:59:29 +0530

    CRUD Operation Completed on Controller class

--- restaurant-info-service/src/main/java/com/example/demo/controllers/RestaurantInfoController.java
+++ restaurant-info-service/src/main/java/com/example/demo/controllers/RestaurantInfoController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,13 +38,20 @@ public class RestaurantInfoController {
 		}
 		
 		@GetMapping
-		@Operation(description = "This method will bring all the Restaurants Information ",
-		  parameters = @Parameter(example = "abc") )
+		@Operation(description = "This method will bring all the Restaurants Information ")
 		public List<RestaurantInfo> getAll(){
 			
 			return this.service.findAll();
 		}
-	
+		
+		@GetMapping(path = "/{id}")
+		@Operation(description = "This method will bring  the Restaurant By its Id ")
+		public RestaurantInfo getById(@PathVariable("id") int id){
+			
+			return this.service.findById(id);
+		}
+		
+		
 		
 		@PostMapping
 		public ResponseEntity<RestaurantInfo>  addInfo(@RequestBody RestaurantInfo entity){
@@ -53,6 +63,33 @@ public class RestaurantInfoController {
 		
 		}
 	
+		@PutMapping
+		public ResponseEntity<RestaurantInfo>  updateInfo(@RequestBody RestaurantInfo entity){
+			
+			RestaurantInfo addedEntity = this.service.update(entity);
+			
+			
+			return ResponseEntity.status(HttpStatus.OK).body(addedEntity);
 		
+		}
+		
+		@DeleteMapping(path = "/{id}")
+		@Operation(description = "This method will Remove   the Restaurant By its Id ")
+		public ResponseEntity<String> removeById(@PathVariable("id") int id){
+			
+			boolean result = this.service.delete(id);
+			
+			
+			String message ="No Element Found";
+			
+			if(result) {
+				message ="One Element Deleted";
+			}
+			
+			return result?ResponseEntity.status(HttpStatus.OK).body(message):
+				ResponseEntity.status(HttpStatus.NO_CONTENT).body(message);
 
+		}
+		
+		
 }
