be9531ebf91550b871749d4fde39fa4f65bc60b8
srivatsan <vatsan_k@hotmail.com>
2021-10-23 12:46:24 +0530

    Update

--- customer-service-reactive-client/src/main/java/com/example/demo/controllers/ClientController.java
+++ customer-service-reactive-client/src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,5 +1,29 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
+
+@RestController
 public class ClientController {
 
+	
+	@Autowired
+	private WebClient client;
+	
+	
+	private static String baseURI="http://CUSTOMER-SERVICE/";
+	
+	
+	@GetMapping(path = "/client/customers")
+	public Flux<String> getCustomers(){
+		
+		String reqURI = baseURI+"customers";
+		
+		return client.get().uri(reqURI).retrieve().bodyToFlux(String.class);
+		
+	}
 }
