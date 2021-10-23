7429ecd90d54535c4b87fa9cf7581da2c6986833
srivatsan <vatsan_k@hotmail.com>
2021-10-23 12:30:46 +0530

    MicroService -Service -A -Customer Service  Added

---
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.model.Customer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	public Customer ramesh() {
+		
+		return new Customer();
+	}
+}
---
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Customer;
+
+@RestController
+public class CustomerController {
+
+	
+	
+	@Autowired
+	private Customer customer;
+	
+	
+	@Value("${server.port}")
+	private String portNumber;
+	
+	
+	@GetMapping(path = "/customers")
+	public Customer getCustomer() {
+		
+		customer.setId(Integer.parseInt(portNumber));
+		customer.setCustomerName("Ramesh");
+		
+		return customer;
+	}
+	
+}
