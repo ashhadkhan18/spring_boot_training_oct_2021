7862489ab8d1170c5ecebffce1d9d42b6b1e1b0b
srivatsan <vatsan_k@hotmail.com>
2021-10-24 13:07:22 +0530

    Security Configuration Added

--- order-delivery-service/src/main/java/com/example/demo/OrderDeliveryServiceApplication.java
+++ order-delivery-service/src/main/java/com/example/demo/OrderDeliveryServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo.repos.OrderRepository;
 import com.example.demo.entity.*;
@@ -17,6 +19,12 @@ public class OrderDeliveryServiceApplication {
 		SpringApplication.run(OrderDeliveryServiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Bean
 	public CommandLineRunner runner() {
 		
--- order-delivery-service/src/main/java/com/example/demo/config/SecurityConfig.java
+++ order-delivery-service/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	
@@ -27,16 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	
 		http.authorizeRequests().antMatchers("/*.html").permitAll()
-		      .antMatchers("/api/v1").authenticated().and().httpBasic();
+		      .antMatchers("/api/v1/**").authenticated().and().httpBasic();
+		
 		
 	}
+
 
 	
 	
 	
-	@Bean
-	public PasswordEncoder endcoder() {
-		
-		return new BCryptPasswordEncoder();
-	}
 }
