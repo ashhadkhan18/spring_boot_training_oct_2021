e60c2581ceb9a1547ec2652829f2f4c2fdc64870
srivatsan <vatsan_k@hotmail.com>
2021-10-24 10:33:26 +0530

    Update

--- order-delivery-service/src/main/java/com/example/demo/OrderDeliveryServiceApplication.java
+++ order-delivery-service/src/main/java/com/example/demo/OrderDeliveryServiceApplication.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.demo.repos.OrderRepository;
+import com.example.demo.entity.*;
 @SpringBootApplication
 public class OrderDeliveryServiceApplication {
 
@@ -10,4 +17,19 @@ public class OrderDeliveryServiceApplication {
 		SpringApplication.run(OrderDeliveryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner() {
+		
+		
+		return new CommandLineRunner() {
+		
+			@Autowired
+			OrderRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new Order(101,"Ramesh","chennai","walnuts",900,LocalDateTime.now()));
+			}
+		};
+	}
 }
