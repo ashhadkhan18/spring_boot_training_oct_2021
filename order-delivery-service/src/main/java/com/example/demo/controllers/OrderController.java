29880bbc901173dbede6789e49db2ff9dd42fcde
srivatsan <vatsan_k@hotmail.com>
2021-10-24 10:18:51 +0530

    Java Coding Completed

--- order-delivery-service/src/main/java/com/example/demo/controllers/OrderController.java
+++ order-delivery-service/src/main/java/com/example/demo/controllers/OrderController.java
@@ -1,5 +1,55 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Order;
+import com.example.demo.services.OrderService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RefreshScope
+@RequestMapping(path = "/api/v1/orders")
+@Slf4j
 public class OrderController {
 
+	
+	
+	public OrderService service;
+
+	public OrderController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	public OrderController(OrderService service) {
+		super();
+		this.service = service;
+		log.debug("Order Controller Initialized");
+	}
+
+	
+	@GetMapping
+	public List<Order> findAll(){
+		
+		log.debug("Order Controller Find All Method Called");
+		return this.service.findAll();
+	}
+	
+	@PostMapping
+	public Order addOrder(@RequestBody Order order) {
+		
+		log.debug("Order Controller Add Method Called");
+		return this.service.addOrder(order);
+	}
+	
 }
--- order-delivery-service/src/main/java/com/example/demo/services/OrderService.java
+++ order-delivery-service/src/main/java/com/example/demo/services/OrderService.java
@@ -1,5 +1,41 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Order;
+import com.example.demo.repos.OrderRepository;
+import java.util.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class OrderService {
 
+	
+	private OrderRepository repo;
+
+	@Autowired
+	public void setRepo(OrderRepository repo) {
+		this.repo = repo;
+		
+		log.info("Repository Injected");
+	}
+	
+	
+	public List<Order> findAll(){
+		
+		log.info("Find All Order Method Called");
+		return this.repo.findAll();
+	}
+	
+	public Order addOrder(Order entity) {
+		
+		log.info("Add Order Method Called");
+		
+		return this.repo.save(entity);
+		
+	}
+	
+	
 }
