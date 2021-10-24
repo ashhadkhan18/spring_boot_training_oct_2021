12d68c060a2eef3131034734a2f4eb94076d7b55
srivatsan <vatsan_k@hotmail.com>
2021-10-24 10:03:54 +0530

    Order Management Service Added

--- customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -2,12 +2,14 @@ package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
 
 @RestController
+@RefreshScope
 public class CustomerController {
 
 	
---
+++ order-delivery-service/Readme.txt
@@ -0,0 +1,8 @@
+1) Spring boot starter project 
+2) Add  lombok,mysql,web,configclient,actuator,spring data jpa
+3) create a entity class Order 
+4) Create a inteface OrderRepository extending JPARepository
+5) Create a Class OrderService
+6) Create a Class OrderController
+7) Add the required entries in the classes and interfaces
+7) update the github repository with directory and its entries
