7fdff27ae32e579fcd5b59eefb1ee0276a56bb60
srivatsan <vatsan_k@hotmail.com>
2021-10-30 16:22:43 +0530

    Cloud Gateway Filter Added

---
+++ cloud-gateway/src/main/java/com/example/demo/filters/MyGlobalFilter.java
@@ -0,0 +1,34 @@
+package com.example.demo.filters;
+
+import java.util.Set;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyGlobalFilter implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+		String requestPath = exchange.getRequest().getPath().toString();
+		
+	        System.out.println("Request path = " + requestPath);
+	        
+	        HttpHeaders headers = exchange.getRequest().getHeaders();
+	        
+	        Set<String> headerNames = headers.keySet();
+	 
+	        headerNames.forEach((header) -> {
+	            System.out.println(header + " " + headers.get(header));
+	        });
+	         return chain.filter(exchange);
+	} 
+	
+
+}
