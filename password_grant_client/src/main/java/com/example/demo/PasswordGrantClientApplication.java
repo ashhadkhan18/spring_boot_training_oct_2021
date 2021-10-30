a55e5b7f04a8ebe1f36e7374e4512d3793254d12
srivatsan <vatsan_k@hotmail.com>
2021-10-30 14:10:27 +0530

    Password grant client with oauth2Template Added

---
+++ password_grant_client/src/main/java/com/example/demo/PasswordGrantClientApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+
+@SpringBootApplication
+public class PasswordGrantClientApplication {
+
+	public static void main(String[] args) {
+		
+	ConfigurableApplicationContext ctx =	 SpringApplication.run(PasswordGrantClientApplication.class, args);
+		
+	OAuth2RestTemplate template =     ctx.getBean(OAuth2RestTemplate.class);
+	        
+	
+	    System.out.println(template.getForObject("http://localhost:4040/api/v1/payments", String.class));
+	    
+		
+	}
+
+	@Bean
+	public OAuth2RestTemplate  template() {
+		
+		ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+
+	     details.setAccessTokenUri("http://localhost:4040/oauth/token");
+	    
+	details.setClientId("myclient");
+	details.setClientSecret("pass123");
+	details.setUsername("india");
+	details.setPassword("india");
+	details.setGrantType("password");
+	 
+	       OAuth2RestTemplate template = new OAuth2RestTemplate(details);
+	           return template;
+
+		
+		
+	}
+}
